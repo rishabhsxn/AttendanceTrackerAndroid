6d27733d0c0fdbede0f91805af1ab6bca70b3088
Poojan Soni <poojansoni.udr@gmail.com>
2020-02-09 13:07:17 +0530

    Completed Geofence on Map using dummy Location

--- GeofenceBuilder/app/src/main/java/com/example/geofencebuilder/MapsActivity.java
+++ GeofenceBuilder/app/src/main/java/com/example/geofencebuilder/MapsActivity.java
@@ -10,6 +10,7 @@ import android.Manifest;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,16 +19,22 @@ import android.widget.Toast;
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.location.GeofencingClient;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -37,10 +44,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final int REQUEST_CODE_BACKGROUND = 200;
     private static final int LOITERING_TIME = 900000;//Dwell interval = 15mins
     private static final int DURATION = 60*60*1000;//geofence existing interval = 1hour
-    private static final float RADIUS = 500.0f;//radius for geofence
+    private static final float RADIUS = 40.0f;//radius for geofence
     private static final String GEOFENCE_REQ_ID = "My Geofence";
-    private static final LatLng manipalLib = new LatLng(26.8953255,75.5293635);
+    private static final LatLng manipalLib = new LatLng(26.8415517,75.565365);
     private PendingIntent geofencePendingIntent;
+
+    ArrayList<Geofence> geofenceArrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,21 +73,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     //Initialize a geofence object with values
     private void createGeofenceObject(){
         Log.i("Info: ","Inside Create object geofence method");
-        geofence = new Geofence.Builder()
+        geofenceArrayList.add(
+                new Geofence.Builder()
                 .setRequestId(GEOFENCE_REQ_ID) // Geofence ID
                 .setCircularRegion( manipalLib.latitude, manipalLib.longitude, RADIUS) // defining fence region
                 .setExpirationDuration( DURATION ) // expiring date
                 // Transition types that it should look for
                 .setTransitionTypes( Geofence.GEOFENCE_TRANSITION_DWELL )
                 .setLoiteringDelay(LOITERING_TIME)
-                .build();
+                .build()
+        );
     }
 
     //Specify geofences and initial triggers
     private GeofencingRequest getGeofencingRequest() {
+        if(geofenceArrayList == null || geofenceArrayList.size() == 0){
+            Toast.makeText(getApplicationContext(), "ArrayList is Empty", Toast.LENGTH_LONG).show();
+            return null;
+        }
         GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
         builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_DWELL); //to reduce spams of continued stay in location
-        builder.addGeofence((geofence));
+        builder.addGeofences(geofenceArrayList);
         return builder.build();
     }
 
@@ -142,38 +157,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
-//    @Override
-//    public void onRequestPermissionsResult(int REQUEST_CODE_BACKGROUND, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        super.onRequestPermissionsResult(REQUEST_CODE_BACKGROUND, permissions, grantResults);
-//
-//        switch (){
-//            case REQUEST_CODE_BACKGROUND:
-//                // if request is cancelled, the result arrays are empty
-//                if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-//                    Log.i("ON REQUEST PERMISSIONS", "START_BACKGROUND () CALLED FROM ON_REQUEST_PERMISSIONS_RESULT");
-//                    getGeofence();
-//                }
-//                else {
-//                    Toast.makeText(getApplicationContext(), "Permission denied", Toast.LENGTH_SHORT).show();
-//                    requestPermissions();
-//                    // write code to display non-functioning of app here
-//                }
-//                break;
-//
-//        }
-//    }
 
 //add a geofence using geofenceclient
     //
     //Note: *** addGeofence will not work if request permissions are ont granted for api 10 or above
     private void getGeofence() {
 
-        if (geofencingClient == null || geofence == null) {
-            Log.i("Info:", "Could not create geofence ob. or client");
-            return;
-        }
+//        if (geofencingClient == null || geofence == null) {
+//            Log.i("Info:", "Could not create geofence ob. or client");
+//            return;
+//        }
 
-        else {
+//        else {
 //            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 if (checkPermissions()) {
                 // permission already given
@@ -196,10 +191,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //                    }
                 }
 //            }
-        }
+//        }
     }
 
-
+//TODO: CHECK GPS AND NETWORK ENABLED FOR HIGH ACCURACY MODE AND A PROMPT FOR USER
 
     //adds geofence
     private void addGeofence(){
@@ -211,6 +206,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         // Geofences added
                         // ...
                         Log.i(TAG, "Successful to ADD");
+                        markerForGeofence(manipalLib);
+                        drawGeofence();
                     }
                 })
                 .addOnFailureListener(this, new OnFailureListener() {
@@ -225,6 +222,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
 
+    // Draw Geofence circle on GoogleMap
+    private Circle geoFenceLimits;
+    private void drawGeofence(){
+        Log.d("Inside func", "drawGeofence()");
+
+        if ( geoFenceLimits != null )
+            geoFenceLimits.remove();
+
+        CircleOptions circleOptions = new CircleOptions()
+                .center( geoFenceMarker.getPosition())
+                .strokeColor(Color.argb(50, 0,0,255))
+                .fillColor( Color.argb(100, 204, 204, 255 ))
+                .radius( RADIUS );
+        geoFenceLimits = mMap.addCircle( circleOptions );
+    }
 
     /**
      * Manipulates the map once available.
@@ -235,6 +247,27 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+
+    private Marker geoFenceMarker;
+    // Create a marker for the geofence creation
+    private void markerForGeofence(LatLng latLng) {
+        Log.i("Marker for Geo", "markerForGeofence(" + latLng + ")");
+        String title = latLng.latitude + ", " + latLng.longitude;
+        // Define marker options
+        MarkerOptions markerOptions = new MarkerOptions()
+                .position(latLng)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+                .title("Geofence Centre static -Library");
+        if (mMap != null) {
+            // Remove last geoFenceMarker
+            if (geoFenceMarker != null)
+                geoFenceMarker.remove();
+
+            geoFenceMarker = mMap.addMarker(markerOptions);
+        }
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -242,7 +275,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Sydney and move the camera
         LatLng hostel = new LatLng(26.8416183,75.5602043);
         mMap.addMarker(new MarkerOptions().position(hostel).title("New Door Hostels"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(hostel));
+        Float zoom = 14f;
+//        mMap.Camera(CameraUpdateFactory.newLatLng(hostel));
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(hostel, zoom);
+        mMap.animateCamera(cameraUpdate);
 
     }
 }
+
+//TODO: In main project also add transition services function to handle intents by broadcast reciever
+//TODO: In main project add remove geofence after 1 hour duration
