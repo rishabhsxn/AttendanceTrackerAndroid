07c43cd16bae5b30b6602f0d7a4b2b57f2300863
Poojan Soni <poojansoni.udr@gmail.com>
2020-04-30 23:57:32 +0530

    Completed registeration of user(email,password), with no email validation.

--- userlogin1/app/src/main/java/com/example/userlogin1/Register.java
+++ userlogin1/app/src/main/java/com/example/userlogin1/Register.java
@@ -1,13 +1,93 @@
 package com.example.userlogin1;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
+EditText rName,rEmail,rPassword;
+Button rSignUp;
+TextView rLogin;
+FirebaseAuth fAuth;
+ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-    }
-}
+
+        //intializing instances
+        rName = findViewById(R.id.registerName);
+        rEmail = findViewById(R.id.registerEmail);
+        rPassword = findViewById(R.id.registerPassword);
+        rSignUp = findViewById(R.id.SignUp);
+        rLogin = findViewById(R.id.login);
+        progressBar = findViewById(R.id.progressBar);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        //need to check if user is already signed in
+        if(fAuth.getCurrentUser()!= null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
+        //when the user clicks register button need to validate the email, < 6 character password.
+        rSignUp.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                String email = rEmail.getText().toString().trim();
+                String password = rPassword.getText().toString().trim();
+
+                //first need to check if user has entered null value
+                if(TextUtils.isEmpty(email)){
+                    rEmail.setError("Email is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    rPassword.setError("Password is required");
+                    return;
+                }
+
+                //Check password length more than 6
+                if(password.length()<6){
+                    rPassword.setError("more than 6 characters required!");
+                    return;
+                }
+
+                //now All CLear, set the Progress Bar to spin
+                progressBar.setVisibility(View.VISIBLE);
+
+                //create user
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            progressBar.setVisibility(View.INVISIBLE);
+                            Toast.makeText(Register.this,"Registered Successfully!",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }else{
+                            progressBar.setVisibility(View.INVISIBLE);
+                            Toast.makeText(Register.this,"Error! "+ task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }); //user created if successful
+            }//onclick End
+        });//buttonOnClick listner ends
+    }//OnCreate Ends
+}//Activity Ends
