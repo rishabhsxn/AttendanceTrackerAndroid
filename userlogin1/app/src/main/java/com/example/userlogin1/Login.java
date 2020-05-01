5b6f182098c1cec51ff33245dd1ea729cd3474d8
Poojan Soni <poojansoni.udr@gmail.com>
2020-05-02 00:34:06 +0530

    Added forgot password option to resend link to registered email

--- userlogin1/app/src/main/java/com/example/userlogin1/Login.java
+++ userlogin1/app/src/main/java/com/example/userlogin1/Login.java
@@ -1,8 +1,10 @@
 package com.example.userlogin1;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -14,6 +16,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -21,7 +25,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class Login extends AppCompatActivity {
     EditText lEmail,lPassword;
     Button lLogin;
-    TextView lRegister;
+    TextView lRegister,forgotPassword;
     FirebaseAuth fAuth;
     ProgressBar progressBar;
 
@@ -34,6 +38,7 @@ public class Login extends AppCompatActivity {
         lPassword = findViewById(R.id.loginPassword);
         lLogin = findViewById(R.id.login);
         lRegister = findViewById(R.id.textRegister);
+        forgotPassword = findViewById(R.id.forgotPassword);
         progressBar = findViewById(R.id.loginprogressBar);
 
         fAuth = FirebaseAuth.getInstance();
@@ -84,6 +89,50 @@ public class Login extends AppCompatActivity {
                         }
                     }
                 }); //user created if successful
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final EditText resetMail = new EditText(v.getContext());
+
+                AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+
+                passwordResetDialog.setTitle("Reset Password?");
+                passwordResetDialog.setMessage("Enter your Email to receive Reset-Link ");
+                passwordResetDialog.setView(resetMail);
+
+                passwordResetDialog.setPositiveButton("Continue", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Extract the email and send Reset LInk
+                        String mail = resetMail.getText().toString();
+                        if(mail.isEmpty()){
+                            resetMail.setError("Required!");
+                            return;
+                        }
+                        fAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(Login.this,"Reset-link sent to your Email",Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Login.this,"Unable to send Reset-link "+ e.getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
+
+                passwordResetDialog.setNegativeButton("Leave", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //redirect to login view
+                    }
+                });
+                passwordResetDialog.create().show();
             }
         });
 
