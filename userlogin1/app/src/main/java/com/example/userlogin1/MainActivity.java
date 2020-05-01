1efda59c280c1671b487583752a780f8a59fe306
Poojan Soni <poojansoni.udr@gmail.com>
2020-05-02 00:04:32 +0530

    successfully fetch data from firestore and display user profile

--- userlogin1/app/src/main/java/com/example/userlogin1/MainActivity.java
+++ userlogin1/app/src/main/java/com/example/userlogin1/MainActivity.java
@@ -1,19 +1,59 @@
 package com.example.userlogin1;
 
+import androidx.annotation.Nullable;
         import androidx.appcompat.app.AppCompatActivity;
 
         import android.content.Intent;
         import android.os.Bundle;
+        import android.util.Log;
         import android.view.View;
+        import android.widget.TextView;
 
         import com.google.firebase.auth.FirebaseAuth;
+        import com.google.firebase.firestore.DocumentReference;
+        import com.google.firebase.firestore.DocumentSnapshot;
+        import com.google.firebase.firestore.EventListener;
+        import com.google.firebase.firestore.FirebaseFirestore;
+        import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MainActivity extends AppCompatActivity {
+TextView name,email;
+
+// to get user id to access particular data from user collection in firestore
+    FirebaseAuth fAuth;
+//for dataRetrieve
+    FirebaseFirestore fStore;
+    String userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.mName);
+        email = findViewById(R.id.mEmail);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userId = fAuth.getCurrentUser().getUid();
+
+        //retrival of data from firestore need to make document reference
+        final DocumentReference documentReference = fStore.collection("users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                //either there will be exception(error) or documentsnapshot successfully retrieved so check exception as it can cause a crash if e exists than snapshot access will lead to crash
+                if (e!=null){
+                    Log.d("DocumentSnapshot","Error:"+e.getMessage());
+                }else {
+                    //getting the string using key defined from the document snapshot
+                    name.setText(documentSnapshot.getString("fullName"));
+                    email.setText(documentSnapshot.getString("email"));
+                }
+            }
+        });
+
     }
 
     public void logout(View view){
